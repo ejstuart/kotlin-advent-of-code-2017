c462bc4709878bf38c4b6b1dd9237c8b32184893
ejstuart <ejstuart2107@gmail.com>
2023-04-03 12:41:14 +1200

    Completed Day 2 - Part 1 and 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,23 +1,51 @@
-//fun main() {
-//    fun part1(input: List<Int>): Int {
-//        for (row: Int in input) {
-//
-//        }
-//    }
-//
-////    fun part2(input: List<String>): Int {
-////        return input.size
-////    }
-//
-//    // test if implementation meets criteria from the description, like:
-////    val testInput = readInput("Day01_test")
-////    check(part1(testInput) == 1)
-////
-////    val input = readInput("Day01")
-////    part1(input).println()
-////    part2(input).println()
-//
-//    val input = listOf(5195, 753, 2468)
-//    part1(input).println()
-//
-//}
+fun main() {
+    fun part1(input: List<String>): Int {
+        var checksum: Int = 0
+
+        for (row: String in input) {
+            val splitRow: List<String> = row.split("\t")
+            val intRow: List<Int> = splitRow.map { it.toInt() }
+            val difference: Int = intRow.max() - intRow.min()
+            checksum += difference
+        }
+
+        return checksum
+    }
+
+    fun part2(input: List<String>): Int {
+
+        var checksum: Int = 0
+
+        for (row: String in input) {
+            val splitRow: List<String> = row.split("\t")
+            val intRow: List<Int> = splitRow.map { it.toInt() }
+            var modulus = 0
+            var division = 0;
+
+            loop@ for ((numeratorIndex, numerator) in intRow.withIndex()) {
+                for ((denominatorIndex, denominator) in intRow.withIndex()) {
+                    if (numeratorIndex == denominatorIndex) continue
+                    modulus = numerator % denominator
+                    if (modulus == 0) {
+                        division = numerator / denominator
+                        break@loop
+                    }
+                }
+            }
+            checksum += division
+        }
+
+
+        return checksum
+    }
+
+//     test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
+
+    val input = readInput("Day02_test")
+    part1(input).println()
+    part2(input).println()
+
+
+}
